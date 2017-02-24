8dd004f721326bbc7ed47e77887dc62420d45e97
gaihao <gaihao@cantv.cn>
2017-02-24 20:41:01 +0800

    20170224:修改va,以适应调试

--- va.py
+++ va.py
@@ -1,6 +1,7 @@
 import commands 
-
-adbs="adb shell"
+import os
+import time
+import multiprocessing as mp
 
 def getDev():
 	headStr = "List of devices attached"
@@ -16,11 +17,47 @@ def getDev():
 		return "no devices"
 	else:
 		res  = subStr.split("\t")
-
 		return res 
+
+def showDev(devs) : 
+	for dev in devs:
+		print "dev1----->" + dev
 
 def getProp():
 	(status,output)=commands.getstatusoutput(adbs + " getprop")
 	return output
 
-print "" + getProp()
+def connDEV() :
+	os.system('adb connect 192.168.1.116')
+
+def stopProcess() :
+	global id_logcat
+
+	id_logcat = os.getpid()
+	os.system('adb shell logcat > vvv.log')
+	time.sleep(1)
+	return id
+
+
+if __name__ == "__main__" :
+	#connect device
+	#connDEV()
+
+	#
+	res = getDev()
+
+	showDev(res)
+
+	#pp = mp.Process(target=stopProcess)
+	#print 'before : ', pp, pp.is_alive()
+
+	#pp.start()
+	#print 'during : ', pp, pp.is_alive()
+
+	#pp.join(timeout=2)
+
+	#pp.terminate()
+	#print 'terminate : ', pp, pp.is_alive()
+
+	#pp.join()
+	#print 'join : ', pp, pp.is_alive()
