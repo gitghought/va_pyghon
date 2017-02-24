0725401b07665cfea122a611a30ab83866b32c6e
gaihao <gaihao@cantv.cn>
2017-02-24 20:40:31 +0800

    20170224:修改vb以适应面向对象编程

--- vb_get_dev.py
+++ vb_get_dev.py
@@ -1,59 +1,87 @@
-
+import io
 import commands 
 import os
 
-def devSplit(dev) :
-	newDev=dev.split('\t')	
-	newDev.pop(1)
+class Dev:
 
-	return str(newDev)
+	devIP = ("192.168.1.116", "192.168.155.5")
 
-def devsSplit(devs):
-	devs = devs.split('\n')
-	new_devs = []
-	i = 0
-	for dev in devs:
-		new_devs.append(devSplit(dev))
-		i=i+1
-	
-	return new_devs
+	def devSplit(self, dev) :
+		newDev=dev.split('\t')	
+		newDev.pop(1)
 
-def getDevs():
-	headStr = "List of devices attached"
-	(status, output) = commands.getstatusoutput("adb devices")
-	output = output.strip("\n")
-	output = output.strip()
-	pos = len(headStr)
-	subStr = output[pos:]
-	subStr= subStr.strip()
-	subStr = subStr.strip("\n")
+		return str(newDev)
 
-	if len(subStr)==0:
-		return "no devices"
-	else:
-		return devsSplit(subStr)
+	def devsSplit(self, devs):
+		devs = devs.split('\n')
+		new_devs = []
+		i = 0
+		for dev in devs:
+			new_devs.append(self.devSplit(dev))
+			i=i+1
+		
+		return new_devs
 
-def showDevs(devs) : 
-	i = 0
-	for dev in devs:
-		print "dev["+ str(i) +"]----->" + dev
-		i=i+1
-def selectDevFromDevs(devs): 
-	if devs.__len__() >= 2:
-		val = input("you choice")
-		print val
-	else:
-		return 0
-	
+	# haven't done yet
+	def confDev (self, fileName) :
+		io.open(fileName,'r')
+		print res
 
+	def getDevs(self):
+		headStr = "List of devices attached"
+		(status, output) = commands.getstatusoutput("adb devices")
+		output = output.strip("\n")
+		output = output.strip()
+		pos = len(headStr)
+		subStr = output[pos:]
+		subStr= subStr.strip()
+		subStr = subStr.strip("\n")
+
+		if len(subStr)==0:
+			return "no devices"
+		else:
+			return self.devsSplit(subStr)
+
+	def showDevs(self, devs) : 
+		i = 0
+		for dev in devs:
+			print "dev["+ str(i) +"]----->" + dev
+			i=i+1
+
+	def selectDevFromDevs(self, devs): 
+		if devs.__len__() >= 2:
+			val = input("you choice")
+			return val
+		else:
+			return 0
+
+	def connect (self) :
+		for ip in self.devIP :
+			print os.system("adb connect " + " " + ip)
+		
+	# return exectly ip 
+	def connDev(self, choice = 0) :
+		connStr = "adb -s "
+		devs = self.getDevs()
+		self.showDevs(devs)
+		#choice = selectDevFromDevs(devs)
+	#:	print "choice = " + str(choice)
+		return devs[choice].lstrip('[').rstrip(']').lstrip('\'').rstrip('\'')
+		
+
+# just for test
 if __name__ == "__main__" :
 	#connect device
 	#connDEV()
 
-	#
-	res = getDevs()
-	print res.__len__()
-	showDevs(res)
-	choice = selectDevFromDevs(res)
+	# test getdev
+	#res = getDevs()
+	#print res.__len__()
+	#showDevs(res)
+	#choice = selectDevFromDevs(res)
 	
+
+#	confDev("devicelist.in")
+	dev = connDev()
+	print type(dev)
 
