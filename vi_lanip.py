efd2193a6eec21e59562af278d5ad31abf42cc08
gitghought <simpleedu.hello@gmail.com>
2017-03-02 10:32:52 +0800

    20170302:实现了获取局域网内所有可用ip地址的功能，暂不能获取mac

--- vi_lanip.py
+++ vi_lanip.py
@@ -1,12 +1,53 @@
-import srp, Ether, ARP, conf
+import platform 
+import sys 
+import os 
+import time 
+import _thread 
+
+
+def get_os(): 
+  os = platform.system() 
+  if os == "Windows": 
+    return "n" 
+  else: 
+    return "c" 
 
 
 
-if __name__ == "__main__":
-	lan = '10.10.10.0/24'
-	  
-	ans, unans = srp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(pdst=lan), timeout=2)
-	for snd, rcv in ans:
-		cur_mac = rcv.sprintf("%Ether.src%")
-		cur_ip  = rcv.sprintf("%ARP.psrc%")
-		print (cur_mac + ' - ' +cur_ip)
+def ping_ip(ip_str): 
+  cmd = ["ping", "-{op}".format(op=get_os()), 
+      "1", ip_str] 
+  output = os.popen(" ".join(cmd)).readlines() 
+   
+  flag = False 
+  for line in list(output): 
+    if not line: 
+      continue 
+    if str(line).upper().find("TTL") >=0: 
+      flag = True 
+      break 
+  if flag: 
+    print ("ip: %s is ok ***"%ip_str)
+  else :
+    print ("ip: %s is not ok ***"%ip_str)
+ 
+
+ 
+def find_ip(ip_prefix): 
+  for i in range(1,256): 
+    ip = '%s.%s'%(ip_prefix,i) 
+    _thread.start_new_thread(ping_ip, (ip,)) 
+    time.sleep(0.1) 
+   
+  
+if __name__ == "__main__": 
+  print ("start time %s"%time.ctime()) 
+  commandargs = sys.argv[1:] 
+  args = "".join(commandargs)   
+   
+  ip_prefix = '.'.join(args.split('.')[:-1]) 
+  find_ip(ip_prefix) 
+  print ("end time %s"%time.ctime())
+
+
+
