dcfcb2d3674235cf0464909c0ea6af76a6309ca6
gitghought <simpleedu.hello@gmail.com>
2017-03-06 20:17:28 +0800

    20170306:实现判断pm进程是否已经运行

--- vg_get_ps.py
+++ vg_get_ps.py
@@ -10,45 +10,34 @@ import multiprocessing
 from vb_get_dev import Dev
 
 class PsCmd:
+	pmFilter = " pm"
+
 	psResultFileName = ".ps.gh"
 	psResultList = []
 
-	def __init__(self, ip):
-		#self.prop = Prop.Prop()
-		self.dev = Dev()
-		self.ip = self.dev.connect(ip)
+	def __init__(this, ip):
+		#this.prop = Prop.Prop()
+		this.dev = Dev()
+		this.ip = this.dev.connect(ip)
 
-	def __getPsCmd_nothread_danger (self, param = "") :
+	def __getPsCmd_nothread_danger (this, param = "") :
 		cmdPreStr = "adb -s "
-		cmdStr = cmdPreStr + self.ip + " shell ps" + " " + param # + " > " + self.psResultFileName
+		cmdStr = cmdPreStr + this.ip + " shell ps" + " " + param # + " > " + this.psResultFileName
 		#os.system(cmdStr)
-		pro = subprocess.Popen(cmdStr, shell = False, stdout = open(self.psResultFileName, 'w'))
+		pro = subprocess.Popen(cmdStr, shell = False, stdout = open(this.psResultFileName, 'w'))
 		time.sleep(2)
 		pro.kill()
 		pro.wait()
 
 
-	def getPsCmdThread(self, param = "") :
-		self.__getPsCmd_nothread_danger(param)
-		return self.readFile()
-			
+	def __getPsCmdThread(this, param = "") :
+		this.__getPsCmd_nothread_danger(param)
+		return this.__readFile()
 
-	def psSplit(self, psses):
-		psses = psses.split('\n')
-		new_ps = []
-		i = 0
-		for ps in psses:
-			if ps.__len__() == 0:
-				continue
-			new_ps.append(ps)
+	# return a list
+	def __readFile (this) :
 
-			i=i+1
-		
-		return new_ps
-
-	def readFile (self) :
-
-		fp = io.open(self.psResultFileName, 'r')
+		fp = io.open(this.psResultFileName, 'r')
 
 		ncont = []
 		try:
@@ -59,10 +48,25 @@ class PsCmd:
 			fp.close()
 
 		return ncont
+	def isPmThread(this):
+		#reg = ''
+		#this.__getPsCmdThread(r' pm')
+		#fp = open(this.psResultFileName, 'r')
+		#content = fp.read();
+		mlist = this.__getPsCmdThread(r'pm')
+		if len(mlist) > 0 :
+			return True
+		else :
+			return False
+
+
+		
 		
 if __name__ == "__main__":
 	ipaddr = sys.argv[1:]
 
 	ps = PsCmd(ipaddr)
-	mlist = ps.getPsCmdThread()
-	UtilStr.show(mlist)
+	#mlist = ps.__getPsCmdThread()
+	#UtilStr.show(mlist)
+	print (ps.isPmThread())
+
