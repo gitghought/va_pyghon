75a3b0a6b5ad8fefa04e950f52f3e5bb75405e03
gitghought <simpleedu.hello@gmail.com>
2017-03-07 13:44:39 +0800

    20170307:添加字符串工具类，用于显示列表、迭代器等

---
+++ util_str.py
@@ -0,0 +1,44 @@
+import re
+
+class UtilStr:
+
+	@classmethod
+	def split_N(cls, mlist):
+		reg = r'[\n]'
+		regS = re.compile(reg)
+
+		new_props = []
+
+		i = 0
+		for content in mlist:
+			if regS.match(content): 
+				continue
+
+			new_props.append(content)
+
+			i+=1
+		#print (new_props)
+
+		return new_props
+
+	@classmethod
+	def show(cls, mlist) :
+		for cont in mlist:
+			print (cont)
+
+	@classmethod
+	def showDictitonary(cls, mDict) :
+		keys = mDict.keys()
+		i = 0
+		print ("##############################")
+		for key in keys :
+			print (""+str(i) + " : " + "%-10s" %"" + key.rjust(10, ' '))
+			i += 1
+		print ("##############################")
+
+if __name__ == "__main__":
+	#UtilStr.split_N("")
+#	UtilStr.show(["vvv", "vvv"])
+	disc = {"a":"a", "b":"b"}
+	UtilStr.showDictitonary(disc)
+
