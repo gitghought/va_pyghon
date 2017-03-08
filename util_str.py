5688a08af3c3c8814875040b9222f2b099e05d80
gitghought <simpleedu.hello@gmail.com>
2017-03-08 15:59:35 +0800

    20170308:添加公共方法，用于轮训显示所有的字典方法，添加全局类变量，用于对出当前菜单，然后返回上一级菜单

--- util_str.py
+++ util_str.py
@@ -45,9 +45,36 @@ class UtilStr:
 			i += 1
 		print ("##############################")
 
+	@classmethod
+	def operations(cls, dics):
+		cls.__ops(dics)
+
+	@classmethod
+	def __getPosOfDictionary(cls, dics, pos) :
+		p = int(pos)
+		keys = dics.keys()
+		i = 0
+		for key in keys :
+			if i == p:
+				return key
+			i+=1
+
+	@classmethod
+	def __ops(cls,dics) :
+		pos = 0
+		#print (this.dics["pm"]())
+		cls.flag = 0
+		while True:
+			if cls.flag :
+				break
+			UtilStr.showDictitonary(dics)
+			choice = input("you choice is : ")
+			print (dics[cls.__getPosOfDictionary(dics, choice)]())
+
 if __name__ == "__main__":
 	#UtilStr.split_N("")
 #	UtilStr.show(["vvv", "vvv"])
-	disc = {"a":"a", "b":"b"}
-	UtilStr.showDictitonary(disc)
+	dics = {"a":"a", "b":"b"}
+	#UtilStr.showDictitonary(disc)
+	UtilStr.operations(dics)
 
