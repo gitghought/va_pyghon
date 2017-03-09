f3f7ecedc37387811333a49f12f3082f1e23ddea
gaihao <gaihao@cantv.cn>
2017-03-09 12:12:31 +0800

    20170309:实现获取本地分支列表的方法

--- vl_repo.py
+++ vl_repo.py
@@ -14,24 +14,34 @@ class Repo :
 	repoPath = "/home/gaihao/b_h2_allwinner/"
 	repoAllwinner = "/home/gaihao/b_h2_allwinner/Allwinner-h2"
 	repolichee = "/home/gaihao/b_h2_allwinner/Allwinner-h2"
+
 	def __init__(this):
 		this.dics["exit"] = exit
 		this.dics["currentBranch"] = this.__repoCurrentBranch
 		this.dics["switchBranch"] = this.__repoSwitchBranch
+		this.dics["localBranch"] = this.__readFileToGetLocalBranch
 
-		this.priv_dics["readfile"] = this.__readFile
+		this.priv_dics["readFileToGetCurrentBranch"] = this.__readFileToGetCurrentBranch
+		this.priv_dics["getAllBranchToFile"] = this.__repoGetAllBranchToFile
 	
 	def __repoSwitchBranch(this):
 		print (os.getcwd())
 		print ("oldpath = " + this.oldPath)
-		
-	def __repoCurrentBranch(this):
-		# currentpath
-		this.oldPath = os.getcwd()
 
-		# distpath
+	""" read all local branch to list"""
+	def __repoLocalBranch(this):
+		this.__repoGetAllBranchToFile()
+	
+		pass
+
+	def __changeToDistPath (this):
 		os.chdir(this.repoPath)
 		newPath = os.getcwd()
+
+		return newPath
+
+	def __repoGetAllBranchToFile(this):
+		this.__changeToDistPath()
 
 		cmdRepo = "repo forall -c "
 		cmdGit = "git branch -a"
@@ -39,21 +49,40 @@ class Repo :
 
 		pro = subprocess.Popen(cmdStr, shell = True, stdout = open(this.branchFile, 'w'))
 		pro.wait()
+		
+	def __repoCurrentBranch(this):
+		# currentpath
+		this.oldPath = os.getcwd()
+
+		this.__repoGetAllBranchToFile()
 
 		# get current branch string
-		curBran = this.__readFileToGetCurrentBranch()
+		this.curBran = this.__readFileToGetCurrentBranch()
 
-		return curBran
+		return this.curBran
 
 	def __readFileToGetCurrentBranch (this):
 		fp = open(this.branchFile, 'r')
 		str = fp.read()
-		#print (str)
 		# create a reg to filter the current branch
 		reg = r'^\*.*'
 		starStart = re.compile(reg, re.M)
 		ll = starStart.findall(str)
 		return ll[0]
+
+	# return local branch into a list
+	def __readFileToGetLocalBranch(this):
+		# change current path to distenation
+		this.__changeToDistPath()
+		fp = open(this.branchFile, 'r')
+		str = fp.read()
+		reg = r'^[ ]{0,}(?!.*remote.*).*'
+		starStart = re.compile(reg, re.M)
+		ll = starStart.findall(str)
+
+		list1=sorted(set(ll),key=ll.index) 
+		UtilStr.show(list1)
+		return list1
 	
 	def select(this) :
 		UtilStr.operations(this.dics)
